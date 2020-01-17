36fdab2b38c00e471662018d21466506e852a851
Valdir Viana <valdir.ferreira@mandic.net.br>
2020-01-17 14:42:32 -0300

    test: binarygap c#

--- 01-binarygap/csharp/Program.cs
+++ 01-binarygap/csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp
 {
@@ -7,6 +9,31 @@ namespace csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // "100101011100000011010"
+            Console.WriteLine(solution(32));
+        }
+
+        public static int solution(int N)
+        {
+            var binary = Convert.ToString(N, 2);
+
+            var gaps = new List<int>();
+            var startedGap = false;
+
+            foreach (var el in binary)
+            {
+                if (el.Equals('1') && !startedGap)
+                {
+                    startedGap = true;
+                    gaps.Add(0);
+                }
+                else if (el.Equals('0') && startedGap) gaps[(gaps.Count - 1)]++;
+                else if (el.Equals('1') && startedGap && gaps.Last() > 0) startedGap = false;
+            }
+
+            if (startedGap && gaps.Count > 0) gaps.RemoveAt(gaps.Count - 1);
+
+            return gaps.Count > 0 ? gaps.Max() : 0;
         }
     }
 }
